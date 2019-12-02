6ff2360b3d3d1b98daf6e894fa6a4f10d613cd1a
maxmellon <tsujiken_melon@r.recruit.co.jp>
2019-12-02 19:04:23 +0900

    feat: Use new Context API

--- src/useAsyncLoader.js
+++ src/useAsyncLoader.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { ReactReduxContext } from 'react-redux';
 import ReduxAsyncLoaderContext from './ReduxAsyncLoaderContext';
+
+const WrapedReduxAsyncLoaderContext = ({ child, renderProps }) => (
+  <ReactReduxContext.Consumer>
+    {({ store }) => (
+      <ReduxAsyncLoaderContext {...renderProps} ctx={{ store }}>
+        {child}
+      </ReduxAsyncLoaderContext>
+    )}
+  </ReactReduxContext.Consumer>
+);
 
 export default function useAsyncLoader() {
   return {
     renderRouterContext: (child, renderProps) => (
-      <ReduxAsyncLoaderContext {...renderProps}>{child}</ReduxAsyncLoaderContext>
+      <WrapedReduxAsyncLoaderContext child={child} renderProps={renderProps} />
     ),
   };
 }
